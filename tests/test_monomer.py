6cd8f34fab3d783eca03e0cc2d6f6358599dfbcc
richard morris <richard@corkmate.com>
2024-04-17 09:37:23 +1000

    fixed 1,4-glucose test

--- tests/test_monomer.py
+++ tests/test_monomer.py
@@ -35,14 +35,16 @@ def test_monomer_GLN(data_dir: Path ):
 def test_monomer_GLU(data_dir: Path ):
     glu = Topology.from_ITP(data_dir/"glucose.itp")
     
-    monomer = Monomer(glu, [Junction("A",bond_a), Junction("C",bond_b)])
+    glu_1 = glu.junction("O6","H5").named("1")
+    glu_4 = glu.junction("C6","O2").named("4")
+    monomer = Monomer(glu, [glu_1, glu_4])
     
-    assert len(monomer.topology.atoms) == 20
+    assert len(monomer.topology.atoms) == 24
     assert len(monomer.junctions.get_junctions()) == 2
-    assert monomer.junctions.named("A")[0].location.atom_a.atom_name == 'N1'
-    assert monomer.junctions.named("A")[0].location.atom_b.atom_name == 'H6'
-    assert monomer.junctions.named("C")[0].location.atom_a.atom_name == 'C4'
-    assert monomer.junctions.named("C")[0].location.atom_b.atom_name == 'O1'
+    assert monomer.junctions.named("1")[0].monomer_atom.atom_name == 'O6'
+    assert monomer.junctions.named("1")[0].residue_atom.atom_name == 'H5'
+    assert monomer.junctions.named("4")[0].monomer_atom.atom_name == 'C6'
+    assert monomer.junctions.named("4")[0].residue_atom.atom_name == 'O2'
     
 def test_serializable(data_dir: Path, output_dir: Path):
     arg = Topology.from_ITP(data_dir/"arginine.itp")
