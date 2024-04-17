b6682ec586f073f5589907973562cbf8554a8ce7
richard morris <richard@corkmate.com>
2024-04-17 10:13:39 +1000

    add a xfailing test to visualize glucose

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+
+import pytest
 from polytop.junction import Junction, Junctions
 from polytop.monomer import Monomer
 from polytop.topology import Topology
@@ -28,3 +30,14 @@ def test_2D_monomer(data_dir: Path, output_dir: Path):
     # assert image was created
     assert os.path.exists(image_path)
 
+@pytest.mark.xfail(reason="Visualize can not infer hydrogen atom types from 'HC' atoms in the glucose topology file.")
+def test_visualize_GLU(data_dir: Path, output_dir: Path):
+    glu = Topology.from_ITP(data_dir/"glucose.itp")
+    
+    glu_1 = glu.junction("C1","O3").named("1")
+    glu_4 = glu.junction("C4","HC3").named("4")
+    glu_6 = glu.junction("C6","HC11").named("6")
+    glu_monomer = Monomer(glu, [glu_1, glu_4, glu_6])
+    image_path = output_dir/'glucose_monomer.png'
+    
+    Visualize.monomer(glu_monomer).draw2D(image_path,size=(800,300),show_legend=True)
