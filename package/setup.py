9fd6995820adb03cc9847148360da6cba4d8fffd
lunamorrow <luna.morrow2@gmail.com>
2024-05-13 14:29:10 +1000

    Fixed README.ipynb

--- package/setup.py
+++ package/setup.py
@@ -3,7 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="polytop",
     version="1.0",
-    # packages=find_packages(),
     packages=["polytop"],
     py_modules=["polytop"],
     install_requires=[
@@ -13,10 +12,6 @@ setup(
         "py3Dmol",
         "rdkit",
     ],
-    entry_points="""
-        [console_scripts]
-        polytop=polytop.cli:cli
-    """,
     python_requires=">=3.10",
 )
 
