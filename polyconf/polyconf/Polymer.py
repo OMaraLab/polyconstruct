470efb7e55a9eb2cd3951e43557dbe0ff92fb08b
lunamorrow <luna.morrow2@gmail.com>
2024-12-12 13:44:50 +1000

    Update PolyConf examples with new import methods and begin to reinstate the gencomp method

--- polyconf/polyconf/Polymer.py
+++ polyconf/polyconf/Polymer.py
@@ -392,62 +392,59 @@ class Polymer:
         # want to extend this to cover more than one dihedral
         return(pairlist)
 
+    def gencomp(mdict, length, fill, middle, count = True, frac = False): 
+        """
+        Generate the linear conformation of a polymer from a given length, 
+        specified monomer composition and a dictionary of monomers.
 
-#   TODO I am wondering if we should deprecate gencomp in favour of example scripts; it's very fussy.  If not, I'll need to document how to make an mdict.
-
-    # def gencomp(mdict, length, fill, middle, count = True, frac = False): 
-    #     """
-    #     Generate the linear conformation of a polymer from a given length, 
-    #     specified monomer composition and a dictionary of monomers.
-
-    #     Args:
-    #         mdict (dict): dictionary of resnames in the middle with how many
-    #                 of each type
-    #         length (int): desired length of the polymer 
-    #                 (i.e. number of monomers)
-    #         fill (list): list of monomers which can be used to build the 
-    #                 polymer middle (i.e. are not terminal monomers) if there 
-    #                 are not enough in 'middle' to complete the monomer
-    #         middle (list): list of monomers to build the polymer from
-    #         count (bool): generate absolute composition with counts (e.g. 4 of
-    #                 monomer A and 12 of monomer B), default is True
-    #         frac (bool): generate relative composition with fractions (e.g. 25%
-    #                 monomer A and 75% monomer B), default is False
+        Args:
+            mdict (dict): dictionary of resnames in the middle with how many
+                    of each type
+            length (int): desired length of the polymer 
+                    (i.e. number of monomers)
+            fill (list): list of monomers which can be used to build the 
+                    polymer middle (i.e. are not terminal monomers) if there 
+                    are not enough in 'middle' to complete the monomer
+            middle (list): list of monomers to build the polymer from
+            count (bool): generate absolute composition with counts (e.g. 4 of
+                    monomer A and 12 of monomer B), default is True
+            frac (bool): generate relative composition with fractions (e.g. 25%
+                    monomer A and 75% monomer B), default is False
         
-    #     Returns:
-    #         polyList: a list with the sequence of randomly ordered monomers of
-    #                 the desired length and composition to build the polymer
-    #     """
-    #     #TODO: is this able to generate instructions for branched polymers?
-    #     polycomp = []
+        Returns:
+            polyList: a list with the sequence of randomly ordered monomers of
+                    the desired length and composition to build the polymer
+        """
+        #TODO: is this able to generate instructions for branched polymers?
+        polycomp = []
 
-    #     if count:
-    #         for m in middle:
-    #             rcount = mdict['count'][m]
-    #             for i in range(rcount):
-    #                 polycomp += [m] # build a list with one of each monomer unit present in the final polymer
+        if count:
+            for m in middle:
+                rcount = mdict['count'][m]
+                for i in range(rcount):
+                    polycomp += [m] # build a list with one of each monomer unit present in the final polymer
 
-    #     elif frac:
-    #         for m in middle:
-    #             rfrac = int(length * mdict['frac'][m]) # fraction of total length that is monomer x
-    #             for i in range(rfrac):
-    #                 polycomp += [m] # build a list with one of each monomer unit present in the final polymer
+        elif frac:
+            for m in middle:
+                rfrac = int(length * mdict['frac'][m]) # fraction of total length that is monomer x
+                for i in range(rfrac):
+                    polycomp += [m] # build a list with one of each monomer unit present in the final polymer
         
-    #     # then randomise positions of monomers
-    #     # 'polycomp' is ordered list of X number of monomers -> will add all of monomer A required, before doing all of B, etc.
+        # then randomise positions of monomers
+        # 'polycomp' is ordered list of X number of monomers -> will add all of monomer A required, before doing all of B, etc.
 
-    #     filler = [x for x in fill]
-    #     while len(polycomp) < length:
-    #         sel = random.sample(filler,1)
-    #         polycomp += sel # if the polymer is shorter than the desired length, continue to fill from the given list 
-    #         filler = [x for x in filler if not x in sel]
-    #         if len(filler) == 0: 
-    #             filler = [x for x in fill]
+        filler = [x for x in fill]
+        while len(polycomp) < length:
+            sel = random.sample(filler,1)
+            polycomp += sel # if the polymer is shorter than the desired length, continue to fill from the given list 
+            filler = [x for x in filler if not x in sel]
+            if len(filler) == 0: 
+                filler = [x for x in fill]
 
-    #     polyList=random.sample(polycomp,length) # reorder the list of monomers randomly
+        polyList=random.sample(polycomp,length) # reorder the list of monomers randomly
 
-    #     polyList[0] = polyList[0][:-1] + 'I' # convert first monomer from middle to initiator -> add extra bit onto middle mono
-    #     # TODO: REWORK THIS TO BUILD IN SAME WAY AS POLYTOP!
+        polyList[0] = polyList[0][:-1] + 'I' # convert first monomer from middle to initiator -> add extra bit onto middle mono
+        # TODO: REWORK THIS TO BUILD IN SAME WAY AS POLYTOP!
 
-    #     polyList[-1] = polyList[-1][:-1] + 'T' # convert last monomer from middle to terminator -> add extra bit onto middle mono
-    #     return(polyList) # list of length items with all monomers in order -> used to build w PDBs
+        polyList[-1] = polyList[-1][:-1] + 'T' # convert last monomer from middle to terminator -> add extra bit onto middle mono
+        return(polyList) # list of length items with all monomers in order -> used to build w PDBs
